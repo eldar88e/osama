04f38a92a2fbf86f14557e05560ec9f59e58a121
Eldar Eminov <em88el@gmail.com>
2025-12-23 02:35:20 +0300

    Refactor

--- app/components/icon_component.rb
+++ app/components/icon_component.rb
@@ -1,10 +1,11 @@
 class IconComponent < ViewComponent::Base
   include ViteRails::TagHelpers
 
-  def initialize(name:, width: 20, height: nil)
+  def initialize(name:, width: 20, height: nil, klass: nil)
     super()
     @name   = name
     @width  = width
     @height = height
+    @klass  = klass
   end
 end
---
+++ app/frontend/javascript/controllers/notices_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = {
+    text: String,
+    timeout: { type: Number, default: 5000 },
+  };
+
+  connect() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    const selector = `[data-notices-text-value="${this.textValue}"]`;
+    const notices = document.querySelectorAll(selector);
+    const lastElement = notices[notices.length - 1];
+
+    notices.forEach((element) => {
+      if (element !== this.element && element !== lastElement) {
+        this.fadeOutAndRemove(element, 50);
+      }
+    });
+
+    this.timer = setInterval(() => {
+      this.fadeOutAndRemove(this.element);
+    }, this.timeoutValue);
+  }
+
+  fadeOutAndRemove(element, time = 1000) {
+    element.classList.add("fade-out");
+    setTimeout(() => {
+      element.remove();
+    }, time);
+  }
+
+  close() {
+    clearInterval(this.timer);
+    this.fadeOutAndRemove(this.element);
+  }
+}
