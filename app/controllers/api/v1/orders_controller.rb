e7d9a0416f89aa646ceb9d339607b66b194e9562
Eldar Eminov <em88el@gmail.com>
2026-01-02 17:17:21 +0300

    Feature(api): add car and client to orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,6 +3,14 @@ module Api
     class OrdersController < Api::V1::ApplicationController
       include Api::ResourceConcern
 
+      def index
+        q = policy_scope(Order).order(:created_at).includes(:user, :car).ransack(params[:q])
+
+        pagy, resources = pagy(q.result)
+
+        render json: { data: OrderSerializer.new(resources), meta: pagy_metadata(pagy) }
+      end
+
       def show
         render json: OrderShowSerializer.new(@resource)
       end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,6 +3,16 @@ class OrderSerializer
 
   root_key :order
 
-  attributes :id, :user_id, :car_id, :state, :paid, :price, :expense, :comment, :appointment_at,
+  attributes :id, :car_id, :state, :paid, :price, :expense, :comment, :appointment_at,
              :processing_at, :completed_at, :cancelled_at, :updated_at, :created_at
+
+  attribute :client_id, &:user_id
+
+  attribute :client do |order|
+    [order.user.first_name, order.user.middle_name].compact.join(' ')
+  end
+
+  attribute :car do |order|
+    [order.car.brand, order.car.model].compact.join(' ')
+  end
 end
