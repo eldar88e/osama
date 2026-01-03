59777ec76b434f0709f9ebccf5f8ea2171c580fb
Eldar Eminov <em88el@gmail.com>
2026-01-03 12:52:22 +0300

    Refactor

--- app/serializers/contactor_serializer.rb
+++ app/serializers/contactor_serializer.rb
@@ -3,7 +3,6 @@ class ContactorSerializer
 
   root_key :user
 
-  attributes :id, :name, :entity_type, :email, :phone, :inn, :kpp, :legal_address, :contact_person,
-             :bank_name, :bik, :checking_account, :correspondent_account, :service_profile,
-             :active, :comment, :created_at
+  attributes :id, :name, :entity_type, :email, :phone, :inn, :kpp, :legal_address, :contact_person, :bank_name,
+             :bik, :checking_account, :correspondent_account, :service_profile, :active, :comment, :created_at
 end
