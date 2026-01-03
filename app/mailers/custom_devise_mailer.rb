eb5e5444e26c8fe1f76f80df347edd4833832a86
Eldar Eminov <em88el@gmail.com>
2026-01-04 01:36:25 +0300

    Refactor

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -6,7 +6,7 @@ class CustomDeviseMailer < Devise::Mailer
 
   default template_path: 'devise/mailer'
 
-  def confirmation_instructions(record, token, opts = {})
+  def confirmation_instructions(record, _token, _opts = {})
     # nothing not send because email is generated with system
     # TODO: Restore confirmation email
     Rails.logger.warn "Confirmation instructions for #{record.email} not sent"
