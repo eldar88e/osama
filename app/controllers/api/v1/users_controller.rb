2336e571a8ecc3395fca0d4c5d20a3a7fa5fe0a5
Eldar Eminov <em88el@gmail.com>
2026-01-03 16:59:58 +0300

    Small fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,7 +9,7 @@ module Api
         base_params = %i[
           email first_name middle_name last_name phone additional_phone company_name inn kpp ogrn legal_address
           actual_address contact_person contact_phone bank_name bik checking_account correspondent_account
-          source comment active role
+          source comment active role position
         ]
         base_params << :password if action_name == 'create'
         params.expect(user: base_params)
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,10 @@
 class UserSerializer
   include Alba::Resource
 
-  # attributes :id, :email
-
   root_key :user
 
   attributes :id, :email, :first_name, :middle_name, :last_name, :full_name, :phone, :additional_phone,
              :company_name, :inn, :kpp, :ogrn, :legal_address, :actual_address, :contact_person, :contact_phone,
-             :bank_name, :bik, :checking_account, :correspondent_account, :source, :comment, :active, :role, :created_at
-
-  # attribute :name_with_email do |resource|
-  #   "#{resource.email}: #{resource.email}"
-  # end
+             :bank_name, :bik, :checking_account, :correspondent_account, :source, :comment, :active,
+             :role, :position, :created_at
 end
