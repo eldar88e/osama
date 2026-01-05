9e6bee9cfde2e3c1fd449df9ccf23925d816b83a
Eldar Eminov <em88el@gmail.com>
2026-01-05 12:38:07 +0300

    Chore(model): add validates and ransackable for Car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,20 @@
 class Car < ApplicationRecord
   belongs_to :owner, class_name: 'User'
 
+  validates :brand, presence: true
+  validates :model, presence: true
   validates :year,
             numericality: {
               only_integer: true,
               greater_than_or_equal_to: 1900,
               less_than_or_equal_to: Time.current.year + 1
             }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id brand model license_plate]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[]
+  end
 end
