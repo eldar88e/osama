a7505e21094346ad08e96b994a85982e536070a0
Eldar Eminov <em88el@gmail.com>
2026-01-05 17:58:16 +0300

    Small fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,15 @@ class OrdersController < ApplicationController
 
   private
 
+  # rubocop:disable Rails/StrongParametersExpect
   def order_params
-    params.expect(order: %i[user_id car_id service_id state paid comment appointment_at])
+    params.require(:order).permit(
+      :user_id, :car_id, :service_id, :state, :paid, :comment :appointment_at,
+      order_items_attributes: %i[
+        id service_id performer_id performer_type state price paid comment materials_price materials_comment
+        delivery_price delivery_comment _destroy
+      ]
+    )
   end
+  # rubocop:enable Rails/StrongParametersExpect
 end
