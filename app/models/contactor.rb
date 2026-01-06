3d1edede25ea80ed139b9799a75ebc5adecc65a0
Eldar Eminov <em88el@gmail.com>
2026-01-06 11:49:21 +0300

    Feature(model): add ransackable to Contactor

--- app/models/contactor.rb
+++ app/models/contactor.rb
@@ -5,4 +5,12 @@ class Contactor < ApplicationRecord
   validates :entity_type, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_blank: true
   validates :inn, length: { in: 10..12 }, allow_blank: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name entity_type]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[]
+  end
 end
