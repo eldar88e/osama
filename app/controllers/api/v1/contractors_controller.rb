02fcfa4eb365beb4515b9cde4bf2683ecc49236d
Eldar Eminov <em88el@gmail.com>
2026-01-10 16:38:10 +0300

    Refactor

--- app/controllers/api/v1/contractors_controller.rb
+++ app/controllers/api/v1/contractors_controller.rb
@@ -7,7 +7,7 @@ module Api
 
       def contractor_params
         params.expect(contractor: %i[name entity_type email phone inn kpp legal_address contact_person bank_name
-                                    bik checking_account correspondent_account service_profile active comment])
+                                     bik checking_account correspondent_account service_profile active comment])
       end
     end
   end
