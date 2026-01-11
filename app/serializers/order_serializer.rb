ac443d23122c616d8836872b70a9b18f055c4c75
Eldar Eminov <em88el@gmail.com>
2026-01-11 16:13:55 +0300

    Small fix

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -6,7 +6,7 @@ class OrderSerializer
   attributes :id, :client_id, :state, :paid, :price, :expense, :comment, :appointment_at,
              :processing_at, :completed_at, :cancelled_at, :updated_at, :created_at
 
-  attribute :client do |order|
-    [order.client.first_name, order.client.middle_name].compact.join(' ')
+  attribute :client_full_name do |order|
+    order.client&.full_name
   end
 end
