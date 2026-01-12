71c7f659292b357a52a5f70d3d7d0bbb9c36da79
Eldar Eminov <em88el@gmail.com>
2026-01-12 18:16:02 +0300

    Small fix

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -11,4 +11,12 @@ class Expense < ApplicationRecord
 
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
   validates :spent_at, presence: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id amount category description spent_at created_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[]
+  end
 end
