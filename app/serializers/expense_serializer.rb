f3b5b8bb4814eb86438d77590ed41e83d645b608
Eldar Eminov <em88el@gmail.com>
2026-01-17 00:52:25 +0300

    Fix

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -3,5 +3,9 @@ class ExpenseSerializer
 
   root_key :expense
 
-  attributes :id, :amount, :expense_category_id, :description, :spent_at, :created_at
+  attributes :id, :amount, :description, :spent_at, :created_at
+
+  attribute :category_title do |expense|
+    expense.category&.title
+  end
 end
