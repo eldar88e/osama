80ed91ede6b2d71344d0e743ccbfc234af26be99
Eldar Eminov <em88el@gmail.com>
2026-01-19 19:05:54 +0300

    Small fix

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -6,4 +6,12 @@ class Investment < ApplicationRecord
 
   # scope :recent, -> { order(invested_at: :desc) }
   # scope :for_period, ->(from, to) { where(invested_at: from..to) }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id amount comment invested_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[]
+  end
 end
