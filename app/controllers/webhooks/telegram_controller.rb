118853e8a0c20716f65fa4b43e22f36406e3a95a
Eldar Eminov <em88el@gmail.com>
2026-01-20 19:02:05 +0300

    Refactor

--- app/controllers/webhooks/telegram_controller.rb
+++ app/controllers/webhooks/telegram_controller.rb
@@ -2,7 +2,6 @@ module Webhooks
   class TelegramController < Webhooks::ApplicationController
     def create
       InboundWebhookJob.perform_later(:telegram, params.to_unsafe_h)
-      # InboundMessageDispatcher.call(source: :telegram, payload: params.to_unsafe_h)
 
       head :ok
     end
