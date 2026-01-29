cdb9755bc8bf753823597d7c88730ac42e161d62
Eldar Eminov <em88el@gmail.com>
2026-01-29 19:05:57 +0300

    Small fix

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -3,6 +3,14 @@ module Api
     class ConversationsController < Api::V1::ApplicationController
       include Api::ResourceConcern
 
+      def index
+        q = policy_scope(resource_class).order(last_message_at: desc).ransack(params[:q])
+
+        pagy, resources = pagy(q.result)
+
+        render json: { data: serializer.new(resources), meta: pagy_metadata(pagy) }
+      end
+
       private
 
       def conversation_params
