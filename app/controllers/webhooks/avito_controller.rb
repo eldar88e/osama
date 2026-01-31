2ce6465cf929c2ed870c1f05309937039107886a
Eldar Eminov <em88el@gmail.com>
2026-02-01 01:50:58 +0300

    Small fix

--- app/controllers/webhooks/avito_controller.rb
+++ app/controllers/webhooks/avito_controller.rb
@@ -2,9 +2,7 @@ module Webhooks
   class AvitoController < Webhooks::ApplicationController
     def create
       Rails.logger.error '=' * 20
-      Rails.logger.error params
-      Rails.logger.error '*' * 20
-      Rails.logger.error request.body.read
+      Rails.logger.warn params
       Rails.logger.error '=' * 20
       InboundWebhookJob.perform_later(:avito, params.to_unsafe_h)
 
