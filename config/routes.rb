090388509a91aaa1b66f22a0175420b50a37ec19
Eldar Eminov <em88el@gmail.com>
2026-02-01 01:48:50 +0300

    Small fix

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   namespace :webhooks do
     resources :telegram, only: :create
-    resource :avito, only: :create
+    resources :avito, only: :create
   end
 
   draw :admin
