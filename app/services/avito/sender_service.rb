f1d1e47a96b0dc555f99ee0f34a030f609022a3b
Eldar Eminov <em88el@gmail.com>
2026-02-01 19:56:14 +0300

    Feature(model): add published_at to Message and avito_id to User

--- app/services/avito/sender_service.rb
+++ app/services/avito/sender_service.rb
@@ -1,7 +1,5 @@
 module Avito
   class SenderService
-    include AvitoConcerns
-
     def initialize(text, chat_id)
       @text = text
       @chat_id = chat_id
@@ -12,6 +10,7 @@ module Avito
     end
 
     def call
+      set_avito
       set_account
       url      = msg_url(1)
       payload  = { message: { text: params[:msg] }, type: 'text' }
@@ -24,5 +23,33 @@ module Avito
     def msg_url(version = 3)
       "https://api.avito.ru/messenger/v#{version}/accounts/#{@account['id']}/chats/#{@chat_id}/messages"
     end
+
+    def set_account
+      @account = fetch_cached(:account_avito, 6.hours, url: 'https://api.avito.ru/core/v1/accounts/self')
+    end
+
+    def fetch_cached(key, expires_in = 5.minutes, **args)
+      result = Rails.cache.fetch(key, expires_in:) do
+        fetch_and_parse(args[:url], args[:method] || :get, args[:payload])
+      end
+      Rails.cache.delete(key) if result.is_a?(Hash) && result.key?(:error)
+      result
+    end
+
+    def fetch_and_parse(url, method = :get, payload = nil)
+      response = @avito.connect_to(url, method, payload)
+      return { 'error' => "Ошибка подключения к API Avito. Статус: #{response&.status}" } unless response&.success?
+
+      JSON.parse(response.body)
+    rescue JSON::ParserError => e
+      { 'error' => "Ошибка парсинга JSON: #{e.message}" }
+    end
+  end
+
+  def set_avito
+    @avito = AvitoService.new
+    return if @avito.token_status.nil? || @avito.token_status == 200
+
+    error_notice t('avito.error.set_avito')
   end
 end
