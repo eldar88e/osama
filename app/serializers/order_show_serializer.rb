d08f9163f14b2c4b2ec1987d6c47bf06ffe687fc
Eldar Eminov <em88el@gmail.com>
2026-02-22 05:59:40 +0300

    Small fix

--- app/serializers/order_show_serializer.rb
+++ app/serializers/order_show_serializer.rb
@@ -4,7 +4,7 @@ class OrderShowSerializer
   root_key :order
 
   attributes :id, :client_id, :state, :paid, :price, :expense, :comment, :appointment_at,
-             :processing_at, :completed_at, :cancelled_at, :updated_at, :created_at
+             :processing_at, :completed_at, :cancelled_at, :updated_at, :created_at, :deposit, :paid_at
 
   attribute :client_full_name do |order|
     order.client&.full_name
