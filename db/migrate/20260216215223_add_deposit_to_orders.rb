e719bf17af64b4ef6dbc4d0cc97500f3402c347e
Eldar Eminov <em88el@gmail.com>
2026-02-22 04:34:41 +0300

    Small fix

--- db/migrate/20260216215223_add_deposit_to_orders.rb
+++ db/migrate/20260216215223_add_deposit_to_orders.rb
@@ -2,6 +2,5 @@ class AddDepositToOrders < ActiveRecord::Migration[8.1]
   def change
     add_column :orders, :deposit, :decimal, precision: 12, scale: 2, default: 0, null: false
     add_column :orders, :paid_at, :datetime, default: nil
-    add_column :orders, :paid, :boolean, default: false, null: false
   end
 end
